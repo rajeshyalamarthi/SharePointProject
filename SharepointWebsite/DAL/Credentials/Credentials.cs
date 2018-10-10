6010f42605c6a6aa651e7afdd95bc44dedb2cda3
Yalamarthi Rajesh <rajeshchowdary3230@gmail.com>
2018-10-10 17:17:18 +0530

    FirstLog

---
+++ SharepointWebsite/DAL/Credentials/Credentials.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+   public class Credentials
+    {
+
+        public string UserName = "rajesh.yalamarthi@acuvate.com";
+        public string Passkey = "Welcome@3230";
+           
+         
+        public SecureString password = GetPassword();
+
+
+
+
+
+
+     private static SecureString GetPassword()
+
+        {
+            ConsoleKeyInfo info;
+            //Get the user's password as a SecureString  
+            SecureString securePassword = new SecureString();
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    securePassword.AppendChar(info.KeyChar);
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return securePassword;
+        }
+    }
+}
---
+++ SharepointWebsite/SharepointWebsite/AddField.aspx.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace SharepointWebsite
+{
+    public partial class AddField : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+              string UserName = "rajesh.yalamarthi@acuvate.com";
+              string Passkey = "Welcome@3230";
+            var Password = new SecureString();
+
+            foreach(char c in Passkey){
+                Password.AppendChar(c);
+
+            }
+
+
+        ClientContext clientContext = new ClientContext("https://acuvatehyd.sharepoint.com/teams/Info");
+            clientContext.Credentials = new SharePointOnlineCredentials(UserName, Password);
+            Web web = clientContext.Web;
+            clientContext.Load(web);
+            List list = web.Lists.GetByTitle("projectcheck");
+            string Name = FieldName.Text;
+            //string Type = FieldType.SelectedValue;
+            string Desc = Description.Text;
+
+            Field field = list.Fields.AddFieldAsXml("<Field DisplayName= '" + Name + "'     Description= '" + Desc + "'   Type='" + FieldType.SelectedValue + "'/>",true,AddFieldOptions.DefaultValue);
+            field.Update();
+            clientContext.ExecuteQuery();
+            //string Addfiled = "<Field Type= " + Type + "/>"+ "<DisplayName= " + Name + "/>"+ "<Description= " + Description+"/>";
+
+
+            //  string Addfiled = "<Field Type='Text' DisplayName = 'Silicon' Name='Silicon'/>";
+
+           
+            //FieldCollection fields = list.Fields;
+            //fields.AddFieldAsXml(Addfiled, true, AddFieldOptions.AddToDefaultContentType);
+
+            //clientContext.ExecuteQuery();
+            
+            Label1.Text = "Added Successfully";
+
+
+
+
+        }
+    }
+}
